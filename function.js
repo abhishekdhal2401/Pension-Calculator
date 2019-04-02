9ebc785789014b6a0649ed5e20637bedb4b8c23d
arijit sahoo <tijira1999sahoo@gmail.com>
2019-04-02 23:28:57 +0530

    added preloader

--- function.js
+++ function.js
@@ -1,3 +1,27 @@
+var preloader;
+  function loadNow(opacity){
+  if(opacity<=0){
+    displayContent();
+  }
+
+  else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function() {
+      loadNow(opacity-0.05)
+    },100);
+  }
+}
+
+function displayContent(){
+  loader.style.display= 'none';
+  document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded",function(){
+  loader = document.getElementById('loading');
+  loadNow(2);
+})
+
 
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
