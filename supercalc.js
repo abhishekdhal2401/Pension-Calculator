9399ca2b10084b8f2d3ecb607fbea54b147e9b0f
Adityesh <adityeshcool@gmail.com>
2019-04-08 00:15:21 +0530

    DA calculation implemented

--- supercalc.js
+++ supercalc.js
@@ -1,6 +1,6 @@
 var agetoRetire = 60;
 
-//YEAR OF RETIRE SPAN ELEMENTS
+//YEAR OF RETIRE INPUT ELEMENTS
 var retireDay = document.getElementById('dayOfRetire');
 var retireMonth = document.getElementById('monthOfRetire');
 var retireYear = document.getElementById('yearOfRetire');
@@ -16,7 +16,7 @@ var dayOfJoin = document.getElementById('dayOfJoin');
 var monthOfJoin = document.getElementById('monthOfJoin');
 var yearOfJoin = document.getElementById('yearOfJoin');
 
-
+// YEAR OF RETIREMENT CALCULATION
 dobDay.addEventListener("change",function(){
 
   retireDay.value = dobDay.value  ;
@@ -34,3 +34,40 @@ dobYear.addEventListener("change",function(){
   retireYear.value = parseInt(dobYear.value)+agetoRetire;
 
 });
+
+//DA %
+var daPercent = document.querySelector("#daPercent");
+
+//SUM OF BASIC PAY AND NPA LAST MONTH
+var lastMonth = document.querySelector("#lastMonth");
+
+
+//SUM OF BASIC PAY AND NPA LAST 10 months
+var tenMonths = document.querySelector("#tenMonths");
+
+
+//DA Rupees value
+var daRupees;
+var daRS = document.getElementById('daRS');
+
+
+
+
+// DA Rupees INPUTS
+
+daPercent.addEventListener("change",compare);
+lastMonth.addEventListener("change",compare);
+tenMonths.addEventListener("change",compare);
+
+function compare(){
+  if(lastMonth.value > tenMonths.value){
+  daRupees = (parseFloat(lastMonth.value) * parseFloat(daPercent.value))/100.0;
+}
+
+else{
+  daRupees = (parseFloat(tenMonths.value) * parseFloat(daPercent.value))/100.0;
+}
+
+    daRS.value = String(daRupees);
+
+}
