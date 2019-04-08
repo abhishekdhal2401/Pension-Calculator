eb4247f9a020e6dc43345e314b5072d38a90b8bc
harisumaid <harisumaid27@gmail.com>
2019-04-08 11:56:13 +0530

    period not qualified as service calculated

--- supercalc.js
+++ supercalc.js
@@ -71,3 +71,58 @@ else{
     daRS.value = String(daRupees);
 
 }
+
+
+//INPUT FIELD FOR NOT qualified
+var month;
+var year;
+var day;
+
+function calctime(){
+year = parseInt(four1y.value) + parseInt(four2y.value) + parseInt(four3y.value) + parseInt(four4y.value) + parseInt(four5y.value) + parseInt(four6y.value) ;
+
+month = parseInt(four1m.value) + parseInt(four2m.value) + parseInt(four3m.value) + parseInt(four4m.value) + parseInt(four5m.value) + parseInt(four6m.value) ;
+
+day = parseInt(four1d.value) + parseInt(four2d.value) + parseInt(four3d.value) + parseInt(four4d.value) + parseInt(four5d.value) + parseInt(four6d.value) ;
+
+year = year + parseInt(month/12);
+month = (month%12) + parseInt(day/31);
+day = day%31;
+
+document.querySelector("#dayNotQual").value= day;
+document.querySelector("#monthNotQual").value= month;
+document.querySelector("#yearNotQual").value= year;
+// console.log(year);
+// console.log(month);
+// console.log(day);
+}
+
+var calc_holiday = document.querySelector(".calc-holiday");
+calc_holiday.addEventListener("change", calctime);
+
+var four1y = document.querySelector("#four-11");
+var four2y = document.querySelector("#four-21");
+var four3y = document.querySelector("#four-31");
+var four4y = document.querySelector("#four-41");
+var four5y = document.querySelector("#four-51");
+var four6y = document.querySelector("#four-61");
+
+var four1m = document.querySelector("#four-12");
+var four2m = document.querySelector("#four-22");
+var four3m = document.querySelector("#four-32");
+var four4m = document.querySelector("#four-42");
+var four5m = document.querySelector("#four-52");
+var four6m = document.querySelector("#four-62");
+
+var four1d = document.querySelector("#four-13");
+var four2d = document.querySelector("#four-23");
+var four3d = document.querySelector("#four-33");
+var four4d = document.querySelector("#four-43");
+var four5d = document.querySelector("#four-53");
+var four6d = document.querySelector("#four-63");
+
+
+for(var i = 1; i<=6;i++){
+  console.log(typeof ("four"+i+"d"));
+
+}
